f1a1d01f1738c32b9f13498e3dd8bf0cd85906cf
formula-x <formula.notify@gmail.com>
2021-07-07 18:08:00 -0400

    limit & market orders

--- Alpaca_API.py
+++ Alpaca_API.py
@@ -1,18 +1,21 @@
-import asyncio, aiohttp, json
+import asyncio, aiohttp, json, requests
 import datetime as datetime
 
 class Alpaca_Account:
     '''This class will have everything to do with the users account including placing orders'''
 
-    def __init__(self, member_id, live_key, live_secret, sandbox_key, sandbox_secret):
+    def __init__(self, member_id, live_key, live_secret, paper_key, paper_secret):
         #initialize variable with params/args
         self.member_id = member_id
         self.live_key = live_key
         self.live_secret = live_secret
-        self.sandbox_key = sandbox_key
-        self.sandbox_secret = sandbox_secret
-        self.live_url = 'https://api.alpaca.markets/'
-        self.paper_url = 'https://paper-api.alpaca.markets/'
+        self.paper_key = paper_key
+        self.paper_secret = paper_secret
+        self.live_headers = {'APCA-API-KEY-ID': self.live_key, 'APCA-API-SECRET-KEY': self.live_secret}
+        self.paper_headers = {'APCA-API-KEY-ID': self.paper_key, 'APCA-API-SECRET-KEY': self.paper_secret}
+        self.live_url = 'https://api.alpaca.markets'
+        self.paper_url = 'https://paper-api.alpaca.markets'
+        self.responses_dict = {} #universal dict for storing responses to send back to user
     
 
     async def get_account(self, live = True):
@@ -20,47 +23,53 @@ class Alpaca_Account:
         live == True, if they choose the paper prefix live == False'''
 
         if live == True:
+            url = f'{self.live_url}/v2/account'
             return
         
         elif live == False:
+            url = f'{self.paper_url}/v2/account'
             return
     
 
-    async def send_order(self, side, symbol, qty, type = 'market', tif = 'gtc', funds = False, live = True):
+    async def send_order(self, side, symbol, qty, limit = 0, tif = 'gtc', live = True):
         
-        #qty of shares in live acct
-        if funds == False and live == True: 
-            if side == 'sell':
-                return
-            
-            elif side == 'buy':
-                return
+        #if limit isn't specified it will default as a market order
+        if limit == 0:
+            data = {'symbol': symbol, 'qty': qty, 'side': side, 'type': 'market', 'time_in_force': tif}
+        else:
+            data = {'symbol': symbol, 'qty': qty, 'side': side, 'type': 'limit', 'time_in_force': tif, 'limit_price': limit}
         
-        #dollar amt worth of shares in live acct
-        elif funds == True and live == True:
-            if side == 'sell':
-                return
+        #live account post request
+        if live == True: 
+            url = f'{self.live_url}/v2/orders'
             
+            if side == 'sell':
+                async with aiohttp.ClientSession(headers=self.live_headers) as session:
+                    async with session.post(url, json=data) as resp:
+                        response = await resp.json() 
+                        print(response)                          
             elif side == 'buy':
-                return
+                async with aiohttp.ClientSession(headers=self.live_headers) as session:
+                    async with session.post(url, json=data) as resp:
+                        response = await resp.json()
+                        print(response)
+               
         
-        #qty of shares in paper acct
-        elif funds == False and live == False:
-            if side == 'sell':
-                return
+        #paper account post request
+        elif live == False:
+            url = f'{self.paper_url}/v2/orders'
             
-            elif side == 'buy':
-                return
-        
-        #dollar amt worth of shares in paper acct
-        elif funds == True and live == False:
             if side == 'sell':
-                return
-            
+                async with aiohttp.ClientSession(headers=self.paper_headers) as session:
+                    async with session.post(url, json=data) as resp:
+                        response = await resp.json() 
+                        print(response)
             elif side == 'buy':
-                return
-        return 
-    
+                async with aiohttp.ClientSession(headers=self.paper_headers) as session:
+                    async with session.post(url, json=data) as resp:
+                        response = await resp.json()
+                        print(response)
+                 
    
     async def get_orders(self, live = True):
         #live account
@@ -105,3 +114,13 @@ class Alpaca_Account:
                     async with session.get(f'{self.paper_url}v2/assets/{symbol.upper()}') as request:
                         await request
         return
+
+
+
+a = Alpaca_Account(1, 'AKONGGIJ6V3OMHMIGHON', 'Bf65kFJS0OixniK71p91GB0EPI0W0YKxAgSLmL7n', 'PKM63NQX8JLSSN76IM6P', 'Xs15aW1jXzLHI2duQ6QjyRNsFtIP34rOEDbCgGH8')
+
+asyncio.run(a.send_order('buy', 'AAPL', 2, 120, live=False))
+# data = {'symbol': 'AAPL', 'qty': 2, 'side': 'buy', 'type': 'market', 'time_in_force': 'gtc'}
+# url = f'{a.paper_url}/v2/orders'
+# r = requests.post(url, headers = a.paper_headers, json=data)
+# print(r.json())
--- Member_Dataframe.py
+++ Member_Dataframe.py
@@ -5,23 +5,23 @@ class Member_Data():
 
     def __init__(self):
         #create dictionary
-        self.user_df = pd.DataFrame({"ID": '', "Name": '', "Live_Key": '', "Live_Secret": '', "Sandbox_Key": '', "Sandbox_Secret": ''}, index = [0])
+        self.user_df = pd.DataFrame({"ID": '', "Name": '', "Live_Key": '', "Live_Secret": '', "Paper_Key": '', "Paper_Secret": ''}, index = [0])
 
 
     #update rows, index, and values
     def update_id(self, id, name):
-        self.user_df = pd.DataFrame(self.user_df.append({"ID": id, "Name": name, "Live_Key": '', "Live_Secret": '', "Sandbox_Key": '', "Sandbox_Secret": ''}, ignore_index=True))
+        self.user_df = pd.DataFrame(self.user_df.append({"ID": id, "Name": name, "Live_Key": '', "Live_Secret": '', "Paper_Key": '', "Paper_Secret": ''}, ignore_index=True))
         self.user_df.index = [item for item in self.user_df['ID']]
         print(self.user_df)
         # print(f'USER DF  {self.user_df}  END USER DF')
         # return self.user_df
     
-    def update_keys(self, id, live_key, live_secret, sandbox_key, sandbox_secret):
+    def update_keys(self, id, live_key, live_secret, paper_key, paper_secret):
         '''id param will only be used for indexing'''
         self.user_df.at[id, 'Live_Key'] = str(live_key)
         self.user_df.at[id, 'Live_Secret'] = str(live_secret)
-        self.user_df.at[id, 'Sandbox_Key'] = str(sandbox_key)
-        self.user_df.at[id, 'Sandbox_Secret'] = str(sandbox_secret)
+        self.user_df.at[id, 'Paper_Key'] = str(paper_key)
+        self.user_df.at[id, 'Paper_Secret'] = str(paper_secret)
         return self.user_df
 
 
