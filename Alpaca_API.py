c96735f141fc4b7eb1cf5764ec47529a4ff4f593
formula-x <formula.notify@gmail.com>
2021-07-12 15:29:01 -0400

    cancel orders

--- Alpaca_API.py
+++ Alpaca_API.py
@@ -18,9 +18,12 @@ class Alpaca_Account:
         self.live_url = 'https://api.alpaca.markets'
         self.paper_url = 'https://paper-api.alpaca.markets'
         self.responses_dict = {} #universal dict for storing responses to send back to user
-        #price data
+        #response data for the json responses of our different functions
         self.entry_price = ''
         self.side = ''
+        self.shares = ''
+        self.order_id = ''
+        self.order_response = '' #for get orders to loop through responses to get orders and their ids
 
 
     async def get_account(self, live=True):
@@ -94,7 +97,7 @@ class Alpaca_Account:
                         response = await resp.json()
                         self.entry_price = self.ask_price
                         print(response)
-                 
+
    
     async def get_orders(self, status='open', live=True):
 
@@ -103,17 +106,17 @@ class Alpaca_Account:
         if live == True:
             async with aiohttp.ClientSession(headers=self.live_headers) as session:
                     async with session.get(f'{self.live_url}/v2/orders', json=data) as resp:
-                        response = await resp.json()
-                        print(response)
+                        self.order_response = await resp.json()
+                        p.pprint(self.order_response)
         
         #paper account
         if live == False:
             async with aiohttp.ClientSession(headers=self.paper_headers) as session:
                     async with session.get(f'{self.paper_url}/v2/orders', json=data) as resp:
-                        response = await resp.json()
-                        p.pprint(response)
+                        self.order_response = await resp.json()
+                        p.pprint(self.order_response)
         
-    
+
     async def get_position(self, symbol, live=True):
 
         #live account
@@ -132,6 +135,7 @@ class Alpaca_Account:
         
         self.entry_price = float(response['avg_entry_price'])
         self.side = response['side']
+        self.shares = float(response['qty'])
     
 
     async def get_all_positions(self, live=True):
@@ -158,6 +162,8 @@ class Alpaca_Account:
 
                 if '%' not in str(qty) and qty != 0:
                     url = f'{self.live_url}/v2/positions/{symbol.upper()}?qty={qty}'
+                elif '%' in str(qty) and qty != 0:
+                    url = f'{self.live_url}/v2/positions/{symbol.upper()}?percentage={qty.strip("%")}'
                 else:
                     url = f'{self.live_url}/v2/positions/{symbol.upper()}'
                 
@@ -171,6 +177,8 @@ class Alpaca_Account:
 
                 if '%' not in str(qty) and qty != 0:
                     url = f'{self.paper_url}/v2/positions/{symbol.upper()}?qty={qty}'
+                elif '%' in str(qty) and qty != 0:
+                    url = f'{self.paper_url}/v2/positions/{symbol.upper()}?percentage={qty.strip("%")}'
                 else:
                     url = f'{self.paper_url}/v2/positions/{symbol.upper()}'
                 
@@ -210,9 +218,8 @@ class Alpaca_Account:
             async with aiohttp.ClientSession(headers=self.paper_headers) as session:
                     async with session.get(f'{self.paper_url}/v2/orders/{id}') as resp:
                         response = await resp.json()
-                        print(response)
-        return
-    
+                        print(response)                      
+
 
     async def get_asset(self, symbol, live = True):
         #live account
@@ -232,7 +239,7 @@ class Alpaca_Account:
 
     async def stop_loss(self, symbol, qty, stop_perc, tif = 'gtc', live = True):
         
-        #check information on open position
+        #check information on open position. This will make self.entry_price == the avg entry price in the position
         await self.get_position(symbol.upper(), live)
         
         #adjust json data based on position info
@@ -240,8 +247,8 @@ class Alpaca_Account:
             data = {'symbol': symbol.upper(), 'qty': qty, 'side': 'sell', 'type': 'stop', 'time_in_force': tif,
                     'stop_price': self.entry_price * stop_perc }
         elif self.side == 'short':
-            {'symbol': symbol.upper(), 'qty': qty, 'side': 'buy', 'type': 'stop', 'time_in_force': tif, 
-            'stop_price': self.entry_price * stop_perc }
+            data = {'symbol': symbol.upper(), 'qty': qty, 'side': 'buy', 'type': 'stop', 'time_in_force': tif, 
+                    'stop_price': self.entry_price * stop_perc }
          
          ###
             
@@ -264,7 +271,87 @@ class Alpaca_Account:
                     print(response)
 
 
+    async def take_profit(self, symbol, stop_perc, qty='all', tif = 'gtc', live = True):
+        
+        #check information on open position. This will make self.entry_price == the avg entry price in the position
+        await self.get_position(symbol.upper(), live)
+
+        #if elif statement to decide between default amount of shares to take profit or a selected amount
+        if qty == 'all':
+            qty = self.shares
+        elif qty != 'all':
+            qty = qty
+        
+        #adjust json data based on position info
+        if self.side == 'long':
+            data = {'symbol': symbol.upper(), 'qty': qty, 'side': 'sell', 'type': 'limit', 'time_in_force': tif,
+                    'limit_price': self.entry_price * stop_perc }
+        elif self.side == 'short':
+            data = {'symbol': symbol.upper(), 'qty': qty, 'side': 'buy', 'type': 'limit', 'time_in_force': tif, 
+                    'limit_price': self.entry_price * stop_perc }
+         
+         ###
+            
+        #live account post request
+        if live == True: 
+            url = f'{self.live_url}/v2/orders'
+        
+            async with aiohttp.ClientSession(headers=self.live_headers) as session:
+                async with session.post(url, json=data) as resp:
+                    response = await resp.json() 
+                    print(response)                          
+
+        #paper account post request
+        elif live == False:
+            url = f'{self.paper_url}/v2/orders'
+
+            async with aiohttp.ClientSession(headers=self.paper_headers) as session:
+                async with session.post(url, json=data) as resp:
+                    response = await resp.json() 
+                    print(response)
+
+
+    async def cancel_orders(self, live = True):
+        #live account
+        if live == True:
+            async with aiohttp.ClientSession(headers=self.live_headers) as session:
+                    async with session.delete(f'{self.live_url}/v2/orders') as resp:
+                        response = await resp.json()
+                        print(response)
+        
+        #paper account
+        if live == False:
+            async with aiohttp.ClientSession(headers=self.paper_headers) as session:
+                    async with session.delete(f'{self.paper_url}/v2/orders') as resp:
+                        response = await resp.json()
+                        print(response)
+
+
+    async def cancel_order(self, symbol, live = True):
+        
+        await self.get_orders('open',live=False)
+        
+        #loop through all the orders to find the order that matches the symbol we are looking for
+        for order in self.order_response:
+            if order['symbol'] == symbol.upper():
+                order_id = order['id']
+        
+        #live account
+        if live == True:
+            async with aiohttp.ClientSession(headers=self.live_headers) as session:
+                    async with session.delete(f'{self.live_url}/v2/orders/{order_id}') as resp:
+                        response = await resp.json()
+                        print(response)
+        
+        #paper account
+        if live == False:
+            async with aiohttp.ClientSession(headers=self.paper_headers) as session:
+                    async with session.delete(f'{self.paper_url}/v2/orders/{order_id}') as resp:
+                        response = await resp.json()
+                        print(response)
+
 
 a = Alpaca_Account(1, l_key, l_secret, p_key, p_secret)
 if __name__ == '__main__':
-    asyncio.run(a.close_position('aapl', live=False))
+    asyncio.run(a.send_order('buy', 'aapl', 5, live=False))
+    asyncio.run(a.cancel_order('aapl',live=False))
