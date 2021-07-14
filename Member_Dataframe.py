d3eec82116e4a3a9ce6b4e147282635629b618b0
formula-x <formula.notify@gmail.com>
2021-07-14 12:10:17 -0400

    pandas to dict then back to pandas

--- Member_Dataframe.py
+++ Member_Dataframe.py
@@ -1,24 +1,29 @@
 import pandas as pd
 import discord as dc
+import asyncio
+import aiofiles
+import json
+from aiocsv import AsyncReader, AsyncDictReader, AsyncWriter, AsyncDictWriter
 
 class Member_Data():
 
     def __init__(self):
         #create dictionary
         self.user_df = pd.DataFrame({"ID": '', "Name": '', "Live_Key": '', "Live_Secret": '', "Paper_Key": '', "Paper_Secret": ''}, index = [0])
-
+        self.user_dictionary ={}
 
     #update rows, index, and values
     def update_id(self, id, name):
         '''This class will be called when a user first joins the server, it will capture their name and id'''
 
         self.user_df = pd.DataFrame(self.user_df.append({"ID": id, "Name": name, "Live_Key": '', "Live_Secret": '', "Paper_Key": '', "Paper_Secret": ''}, ignore_index=True))
+
+        #set the index to the user's id
         self.user_df.index = [item for item in self.user_df['ID']]
-        print(self.user_df)
-        # print(f'USER DF  {self.user_df}  END USER DF')
-        # return self.user_df
+        return self.user_df
+        
     
-    def update_keys(self, id, live_key, live_secret, paper_key, paper_secret):
+    async def update_keys(self, id, live_key, live_secret, paper_key, paper_secret):
         '''id param will only be used for indexing
         This class will be called when a user dms the bot the proper command to store its alpaca keys. it will find 
         them in the dataframe by id and add the alpaca keys to  the proper panda column via arg order.
@@ -27,15 +32,29 @@ class Member_Data():
         self.user_df.at[id, 'Live_Secret'] = str(live_secret)
         self.user_df.at[id, 'Paper_Key'] = str(paper_key)
         self.user_df.at[id, 'Paper_Secret'] = str(paper_secret)
-        return self.user_df
+
+        #convert dataframe into dict to asynchronously write it to a csv file
+        self.user_dictionary = self.user_df.to_dict()
+        await self.update_csv()
+        return self.user_df.to_dict()
+    
+    async def read_csv(self):
+        async with aiofiles.open('members_alpaca.csv', 'r') as f:
+            await f.read(json.loads(self.user_dictionary))
+    
+    async def update_csv(self):
+        async with aiofiles.open('members_alpaca.csv', 'w') as f:
+            await f.write(f'{json.dumps(self.user_dictionary)}\n')
 
 
 
 users = Member_Data()
 # print(users.user_df)
 (users.update_id(123, 'george'))
+(users.update_id(124, 'george'))
 print('\n')
-print(users.update_keys(123, 1, 1, 1, 1))
+asyncio.run(users.update_keys(123, 1, 1, 1, 1))
+asyncio.run(users.update_keys(124, 1, 1, 1, 1))
 # users.base_df()
 # print(users.updated_df('g'))
 # print(users.updated_df('gg'))
