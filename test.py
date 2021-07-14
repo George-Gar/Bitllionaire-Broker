2c22658b5f8c4c8b6d855a05859745b227b92ae3
formula-x <formula.notify@gmail.com>
2021-07-14 18:56:07 -0400

    delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from Alpaca_API import a   
-import asyncio, aiohttp
-
-
-# asyncio.run(a.send_order('buy', 'AAPL', 2, live=False))
-print(a.entry_price)
-# asyncio.run(a.stop_loss('aapl', 2, .90, live=False))
